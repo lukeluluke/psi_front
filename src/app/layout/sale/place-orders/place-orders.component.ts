5f93b9bf38b9c3624ee6379623d452d0bd2a6c5a
xiao-cui <cxstam@gmail.com>
2017-11-14 21:27:02 +1100

    Reinitial sale module

--- src/app/layout/sale/place-orders/place-orders.component.ts
+++ src/app/layout/sale/place-orders/place-orders.component.ts
@@ -1,47 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { routerTransition } from '../../../router.animations';
+import {Component, OnInit} from '@angular/core';
+import {routerTransition} from '../../../router.animations';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Companies } from '../../../shared/mock/mock-company';
+import { Products } from '../../../shared/mock/mock-product'
+import { Warehouses} from '../../../shared/mock/mock-warehouse';
+import { Divisions } from '../../../shared/mock/mock-division';
+import { Users } from '../../../shared/mock/mock-user';
+import { PaginationInstance } from 'ngx-pagination';
 
 @Component({
-  selector: 'app-place-orders',
-  templateUrl: './place-orders.component.html',
-  styleUrls: ['./place-orders.component.scss'],
-  animations: [routerTransition()]
+    selector: 'app-place-orders',
+    templateUrl: './place-orders.component.html',
+    styleUrls: ['./place-orders.component.scss'],
+    animations: [routerTransition()]
 })
 export class PlaceOrdersComponent implements OnInit {
-  public companies = [
-      { id: 1, text: 'SWISSE' },
-      { id: 2, text: 'BLACKMORE' },
-      { id: 3, text: 'HEALTHY CARE' },
-      { id: 4, text: 'BIO ISLAND' }
-  ];
-  public users = [
-      { id: 1, text: '张一' },
-      { id: 2, text: '李二' },
-      { id: 3, text: '王三' },
-      { id: 4, text: '赵四' }
-  ];
-  public divisions = [
-      { id: 1, text: '销售' },
-      { id: 2, text: '仓库' },
-      { id: 3, text: '财务' },
-  ];
-  public warehouses = [
-      { id: 1, text: 'Clayton' },
-      { id: 2, text: 'Chadstone' },
-  ];
+    closeResult: string;
+    public pageConfig: PaginationInstance = {
+        id: 'advanced',
+        itemsPerPage: 3,
+        currentPage: 1
+    };
+    public maxSize = 7;
+    public products = Products;
+    public companies = Companies;
+    public users = Users;
+    public divisions = Divisions;
+    public warehouses = Warehouses;
+    public orderProducts = [];
 
-  private value: string;
+    private value: string;
+    constructor(
+        private modalService: NgbModal
+    ) {
+    }
 
-  public selected(value: any): void {
-      console.log('Selected value is: ', value);
-  }
+    ngOnInit() {
+    }
+    open(content) {
+        this.modalService.open(content).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return  `with: ${reason}`;
+        }
+    }
+    public selected(value: any): void {
+        console.log('Selected value is: ', value);
+    }
 
-  public refreshValue(value: any): void {
-      this.value = value;
-  }
-  constructor() { }
+    public removed(value: any): void {
+        console.log('Removed value is: ', value);
+    }
 
-  ngOnInit() {
-  }
+    public typed(value: any): void {
+        console.log('New search input: ', value);
+    }
 
+    public refreshValue(value: any): void {
+        this.value = value;
+    }
+    public addProductItem(id: number): void {
+        const product = this.products.filter(x => x.id === id);
+        if (product) {
+            const productLength = this.orderProducts.length;
+            const selectProduct = {};
+            Object.assign(selectProduct, product[0]);
+            selectProduct['price'] = this.orderProducts.length;
+            selectProduct['row'] = productLength + 1;
+            this.orderProducts.push(selectProduct);
+        }
+    }
 }
