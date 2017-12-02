7ac107c00e451e5b8318c4c5899d424e342829ee
luke <lukeluluke@gmail.com>
2017-12-02 15:54:16 +1100

    Add filters function for order list

--- src/app/layout/purchase/purchase.component.ts
+++ src/app/layout/purchase/purchase.component.ts
@@ -25,10 +25,14 @@ export class PurchaseComponent implements OnInit {
     users = [];
     divisions = [];
     warehouses = [];
+
+    filterCompanyUuid: string = '';
+    filterUserUuid: string = '';
+    filterWarehouseUuid: string = '';
+    filterDivisionUuid: string = '';
     constructor() {
         this.orders = [];
         this.companies = this.convertSelectOptions(Companies);
-        this.users = this.convertSelectOptions(Users);
         this.divisions = this.convertSelectOptions((Divisions));
         this.warehouses = this.convertSelectOptions(Warehouses);
 
@@ -38,6 +42,11 @@ export class PurchaseComponent implements OnInit {
             option['text'] = user.lastName + ' ' + user.firstName ;
             this.users.push(option);
         }
+        const defaultUserOption = {
+            id: -1,
+            text: '显示所有'
+        };
+        this.users.push(defaultUserOption);
 
         this.filterOrders = [];
         this.tempFilterOrders = [];
@@ -50,7 +59,6 @@ export class PurchaseComponent implements OnInit {
         this.countPurchaseOrder = this.filterOrderByType(Order.TYPE_PURCHASE).length;
         this.countReturnOrder = this.filterOrderByType(Order.TYPE_PURCHASE_RETURN).length;
         Object.assign(this.filterOrders, this.orders);
-        console.log(this.orders);
         Object.assign(this.tempFilterOrders, this.orders);
     }
 
@@ -61,28 +69,71 @@ export class PurchaseComponent implements OnInit {
         return this.orders.filter(o => o.type === type);
     }
 
-    private filterOrderByStatus(status: number) {
-        return this.orders.filter(o => o.status === status);
-    }
-
     public filterUser( value ) {
-
+        this.filterUserUuid = (value.id !== -1) ? value.id : '';
+        this.filterOrder();
     }
 
     public filterWarehouse(value) {
-
+        this.filterWarehouseUuid = (value.id !== -1) ? value.id : '';
+        this.filterOrder();
     }
 
     public filterCompany(value) {
-
+        this.filterCompanyUuid = (value.id !== -1) ? value.id : '';
+        this.filterOrder();
     }
 
     public filterDivision(value) {
+        this.filterDivisionUuid = (value.id !== -1) ? value.id : '';
+        this.filterOrder();
+    }
 
+    private filterOrder() {
+        let allOrders = [];
+        Object.assign(allOrders, this.orders);
+        if (this.filterUserUuid !== '') {
+            const userOrders = allOrders.filter( o => o.user.uuid === this.filterUserUuid);
+            allOrders = [];
+            Object.assign(allOrders, userOrders);
+        }
+
+        if (this.filterCompanyUuid !== '') {
+            const companyOrders = allOrders.filter( o => o.company.uuid === this.filterCompanyUuid);
+            allOrders = [];
+            Object.assign(allOrders, companyOrders);
+        }
+
+        if (this.filterDivisionUuid !== '') {
+            const divisionOrders = allOrders.filter( o => o.division.uuid === this.filterDivisionUuid);
+            allOrders = [];
+            Object.assign(allOrders, divisionOrders);
+        }
+
+        if (this.filterWarehouseUuid !== '') {
+            const warehouseOrders = allOrders.filter( o => o.warehouse.uuid === this.filterWarehouseUuid);
+            allOrders = [];
+            Object.assign(allOrders, warehouseOrders);
+        }
+
+        this.filterOrders = [];
+        Object.assign(this.filterOrders, allOrders);
     }
 
     public refreshValue(value) {
-        console.log(value);
+    }
+
+    public removeCompany(value) {
+        this.filterCompanyUuid = '';
+    }
+    public removeUser(value) {
+        this.filterUserUuid = '';
+    }
+    public removeDivision(value) {
+        this.filterDivisionUuid = '';
+    }
+    public removeWarehouse(value) {
+        this.filterWarehouseUuid = '';
     }
 
     /**
@@ -98,6 +149,12 @@ export class PurchaseComponent implements OnInit {
             option['text'] = obj.name;
             options.push(option);
         }
+
+        const defaultOption = {
+            id: -1,
+            text: '显示所有'
+        };
+        options.push(defaultOption);
         return options;
     }
 
